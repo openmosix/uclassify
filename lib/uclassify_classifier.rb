d7f0b3b294055b7fbe77fa0c724fd943cdc44fca
Luca Bonmassar <luca.bonmassar@gmail.com>
2012-06-19 01:53:56 +0200

    support for classifier username

--- lib/uclassify_classifier.rb
+++ lib/uclassify_classifier.rb
@@ -1,13 +1,15 @@
 class UClassifyClassifier
-  def initialize (classifier_name,text)
+  def initialize (classifier_name,text,username=nil)
     @classifier_name = classifier_name
     @text = text
+    @username = username
   end
   
   def to_xml_node xml_document
     classifier_node = Nokogiri::XML::Node.new('classify',xml_document)
     classifier_node['id'] = UClassifyUtils.string_to_id(@classifier_name)
     classifier_node['classifierName'] = @classifier_name
+    classifier_name['username'] = @username unless @username.nil?
     classifier_node['textId'] = UClassifyUtils.string_to_id(@text)
     classifier_node
   end
