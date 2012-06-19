f9cded6feeb0e70f9de971ed158baa9e4bb8fd90
Luca Bonmassar <luca.bonmassar@gmail.com>
2012-06-20 00:31:26 +0200

    typo

--- lib/uclassify_classifier.rb
+++ lib/uclassify_classifier.rb
@@ -9,7 +9,7 @@ class UClassifyClassifier
     classifier_node = Nokogiri::XML::Node.new('classify',xml_document)
     classifier_node['id'] = UClassifyUtils.string_to_id(@classifier_name)
     classifier_node['classifierName'] = @classifier_name
-    classifier_name['username'] = @username unless @username.nil?
+    classifier_node['username'] = @username unless @username.nil?
     classifier_node['textId'] = UClassifyUtils.string_to_id(@text)
     classifier_node
   end
